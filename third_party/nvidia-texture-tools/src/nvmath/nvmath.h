0cd30297a68fe4055966a09c4a4e2868a0779a26
Nan Zhang <zhangnan@baysenvision.com>
2021-06-15 15:45:29 +0800

    nvtt compiles under release

--- third_party/nvidia-texture-tools/src/nvmath/nvmath.h
+++ third_party/nvidia-texture-tools/src/nvmath/nvmath.h
@@ -117,17 +117,17 @@ inline float asinf_assert(const float f)
 #define asinf asinf_assert
 #endif
 
-#if NV_CC_MSVC
-NV_FORCEINLINE float log2f(float x)
-{
-    nvCheck(x >= 0);
-    return logf(x) / logf(2.0f);
-}
-NV_FORCEINLINE float exp2f(float x)
-{
-    return powf(2.0f, x);
-}
-#endif
+// #if NV_CC_MSVC
+// NV_FORCEINLINE float log2f(float x)
+// {
+//     nvCheck(x >= 0);
+//     return logf(x) / logf(2.0f);
+// }
+// NV_FORCEINLINE float exp2f(float x)
+// {
+//     return powf(2.0f, x);
+// }
+// #endif
 
 namespace nv
 {
